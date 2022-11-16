3ca0968cafbad8d87c8625592cfebe4c186a1421
ali-k123 <0alikarami0@gmail.com>
2022-11-16 14:48:52 +0330

    clean up main

--- main.py
+++ main.py
@@ -7,11 +7,3 @@ app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(tasks.router)
-
-#app.mount("/static", StaticFiles(directory="static"), name="static")
-
-#INDEXFILE = open('static/index.html','r').read()
-
-#@app.get("/", response_class=HTMLResponse)
-#async def index():
-#    return INDEXFILE
